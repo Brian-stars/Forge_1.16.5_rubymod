05df1f8f5b20646fdc12a78e7ff1a2f1973fd262
Brian-stars <xingchen_game01@163.com>
2024-07-12 23:39:10 +0800

    version-1.0

---
+++ src/main/java/com/brianstars/rubymod/Main.java
@@ -0,0 +1,26 @@
+package com.brianstars.rubymod;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import static com.brianstars.rubymod.Units.MOD_ID;
+
+@Mod(MOD_ID)
+public class Main {
+    public Main() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.register(this.getClass());
+        bus.addListener(this::CommonSetup);
+        bus.addListener(this::ClientSetup);
+    }
+
+    private void CommonSetup(final FMLCommonSetupEvent event) {
+
+    }
+    private void ClientSetup(final FMLClientSetupEvent event) {
+
+    }
+}
---
+++ src/main/java/com/brianstars/rubymod/Units.java
@@ -0,0 +1,6 @@
+package com.brianstars.rubymod;
+
+public class Units {
+    //设置一个变量MOD_ID
+    public static final String MOD_ID = "rubymod";
+}
